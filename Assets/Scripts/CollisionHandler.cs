60fe133164acdd5c0597ef4e0fce67547b3c8b67
Gutu_Nicolae <nicolausclauss@gmail.com>
2022-08-29 19:37:30 +0300

    Collision added

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,27 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Landing Pad");
+                break;
+            case "Finish":
+                Debug.Log("Finish");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
